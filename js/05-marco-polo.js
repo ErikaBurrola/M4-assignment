f0fdb977ee2cb2002e9bd6a65729c13df7ec3857
Erika Burrola <erika.burrola@aideguru.com>
2024-10-16 19:29:13 -0700

    M4-assigment

--- js/05-marco-polo.js
+++ js/05-marco-polo.js
@@ -1,10 +1,13 @@
 let marco="";
 let polo=""; 
+let marcoPolo ="";
 for(let i=1; i<=100; i++){
 
     marco = i%3 == 0? i+"Marco! ":"";
     document.write('</h>'+marco+'</h>');
     polo = i%5 == 0? i+"Polo! ":"";
     document.write('</h>'+polo+'</h>');
+    marcoPolo = i%15== 0? i+"Marco! Polo! ":"";
+    document.write('</h>'+marcoPolo+'</h>');
     
 }
